e3590df997e4b0d78b083565b2c687e89a7b8f8c
dibss <ludovicad01@gmail.com>
2022-03-10 13:28:07 +0100

    files

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// costo biglietto
+/*let km = Number(prompt("Quanti chilometri vuoi percorrere?"))
+while (Number.isNaN(km)){
+  alert("Inserisci un numero per i chilometri!")
+  km = Number(prompt("Quanti chilometri vuoi percorrere?"))
+}
+
+let userAge = Number(prompt("Inserisci la tua età"))
+while (Number.isNaN(userAge)){
+  alert("Inserisci un numero per la tua età!")
+  userAge = Number(prompt("Inserisci la tua età"))
+}
+
+let kmPrice = 0.21 * km
+
+if (userAge < 18) {
+  kmPrice -= (kmPrice * 0.2);
+  console.log(kmPrice);
+} else if (userAge >= 65) {
+  kmPrice -= (kmPrice * 0.4);
+} else { 
+  kmPrice;
+}
+
+document.getElementById("km").innerHTML = km;
+document.getElementById("eta").innerHTML = userAge;
+document.getElementById("price").innerHTML = kmPrice.toFixed(2);*/
