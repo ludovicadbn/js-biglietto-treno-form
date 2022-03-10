d712e93f7b569558037011ec33186ba0c8f7acbf
dibss <ludovicad01@gmail.com>
2022-03-10 17:23:43 +0100

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,53 @@
-// costo biglietto
-/*let km = Number(prompt("Quanti chilometri vuoi percorrere?"))
-while (Number.isNaN(km)){
-  alert("Inserisci un numero per i chilometri!")
-  km = Number(prompt("Quanti chilometri vuoi percorrere?"))
-}
+// button
+const genera = document.getElementById("genera");
+genera.addEventListener("click",
+  function ticket(){
+    const ticket = document.getElementById("ticket");
+    ticket.className = "d-unset";
+  }
+);
 
-let userAge = Number(prompt("Inserisci la tua età"))
-while (Number.isNaN(userAge)){
-  alert("Inserisci un numero per la tua età!")
-  userAge = Number(prompt("Inserisci la tua età"))
-}
+genera.addEventListener("click",
+  function getName() {
+    const userName = document.querySelector("input").value;
+    document.getElementById("name").innerHTML = userName;
+  }
+);
 
-let kmPrice = 0.21 * km
+genera.addEventListener("click",
+  function getName() {
+    const select = document.getElementById("age");
+    let text = select.options[select.selectedIndex].text;
+    let typeOfTicket = "";
+    const km = document.getElementById("km").value; 
+    let kmPrice = 0.21 * km
 
-if (userAge < 18) {
-  kmPrice -= (kmPrice * 0.2);
-  console.log(kmPrice);
-} else if (userAge >= 65) {
-  kmPrice -= (kmPrice * 0.4);
-} else { 
-  kmPrice;
-}
+    if (text == "Under 18"){
+      typeOfTicket += "Biglietto Under 18";
+      kmPrice -= (kmPrice * 0.2);
+    } else if (text == "Over 65"){
+      typeOfTicket += "Biglietto Over 65";
+      kmPrice -= (kmPrice * 0.4);
+    } else {
+      typeOfTicket += "Biglietto Standard";
+      kmPrice;
+    }
 
-document.getElementById("km").innerHTML = km;
-document.getElementById("eta").innerHTML = userAge;
-document.getElementById("price").innerHTML = kmPrice.toFixed(2);*/
+    document.getElementById("typeofticket").innerHTML = typeOfTicket;
+    document.getElementById("price").innerHTML = kmPrice.toFixed(2) + " &#8364;";
+
+    // carrozza
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 50 + 1);
+
+    // codice CP
+    document.getElementById("cp").innerHTML = Math.floor(Math.random() * 100000 + 1);
+  }
+);
+
+const annulla = document.getElementById("annulla");
+annulla.addEventListener("click",
+  function ticket(){
+    const ticket = document.getElementById("ticket");
+    ticket.className = "d-none";
+  }
+);
